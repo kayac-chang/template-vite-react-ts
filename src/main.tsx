31a6d9bcb1098dd9517b04348e6cb258233d82d8
kayac-chang <egg734631@gmail.com>
2021-12-17 16:29:56 +0800

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+
+const element = document.getElementById("root");
+if (!element) {
+  throw new Error("should include #root element in your html document.");
+}
+
+ReactDOM.createRoot(element).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
